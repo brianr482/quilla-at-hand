d121a1969a9bc909f7ccb63537ec4017eb7eae4b
Brian Ramirez <brians@uninorte.edu.co>
2019-11-04 15:07:22 -0500

    feat: Create toast notification component

---
+++ src/modules/utils/ToastNotification.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles((theme) => ({
+  close: {
+    padding: theme.spacing(0.5),
+  },
+}));
+
+export default function ToastNotification({ message }) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      ContentProps={{
+        'aria-describedby': 'notification',
+      }}
+      message={<span id="message-id">{message}</span>}
+      action={[
+        <IconButton
+          key="close"
+          aria-label="close"
+          color="inherit"
+          className={classes.close}
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>,
+      ]}
+    />
+  );
+}
+
+ToastNotification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
